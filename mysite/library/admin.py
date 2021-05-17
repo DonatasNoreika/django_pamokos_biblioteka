b943bb162fa971748e6fcec338a91829aedccc10
Donoras <donoras@gmail.com>
2021-05-17 09:48:00 +0300

    10-1 done

--- mysite/library/admin.py
+++ mysite/library/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Author, Genre, Book, BookInstance, BookReview
+from .models import Author, Genre, Book, BookInstance, BookReview, Profilis
 
 
 # Register your models here.
@@ -43,3 +43,4 @@ admin.site.register(Genre)
 admin.site.register(Book, BookAdmin)
 admin.site.register(BookInstance, BookInstanceAdmin)
 admin.site.register(BookReview, BookReviewAdmin)
+admin.site.register(Profilis)
