0173c77aa7291acfa7591de49c6bbce0642bbed3
Donoras <donoras@gmail.com>
2021-05-19 12:49:29 +0300

    12 url smallfix4

--- mysite/library/views.py
+++ mysite/library/views.py
@@ -189,7 +189,7 @@ def register(request):
                 else:
                     # jeigu viskas tvarkoje, sukuriame naują vartotoją
                     User.objects.create_user(username=username, email=email, password=password)
-                    return redirect(request.GET.get('next'))
+                    return redirect('login')
         else:
             messages.error(request, _('Passwords do not match!'))
             return redirect('register')
